2e09d57b5d168e7e9f96dc0fe17a2a860583008e
Litchstarken <johnjcastillo@gmail.com>
2019-11-13 23:02:29 -0500

    first commit

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./card.styles.css";
+
+export const Card  = (props) => {
+    console.log('monster id', props.monster.id)
+    return (
+        <div className="card-container">
+            <img 
+            alt="monster" 
+            src={`https://robohash.org/${props.monster.id}?set=set3&size=180x180`}
+            />
+            <h2>{props.monster.name}</h2>
+            <p>{props.monster.email}</p>
+        </div>
+    );
+};
+
+export default Card;
