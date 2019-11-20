e2e5e32451521ea553ccfd157d32b83f8117a6a6
Litchstarken <johnjcastillo@gmail.com>
2019-11-20 16:56:17 -0500

    Updates

---
+++ precache-manifest.60f86d99ca351886331fbbbab5733b8c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a31103dab8ca34c11212cbbb60d31dec",
+    "url": "/monters-and-stuff/index.html"
+  },
+  {
+    "revision": "614dcbf33bf1698eddb3",
+    "url": "/monters-and-stuff/static/css/main.c473b1f7.chunk.css"
+  },
+  {
+    "revision": "dbd5996561d449190a43",
+    "url": "/monters-and-stuff/static/js/2.2bc65652.chunk.js"
+  },
+  {
+    "revision": "614dcbf33bf1698eddb3",
+    "url": "/monters-and-stuff/static/js/main.72d4cc5d.chunk.js"
+  },
+  {
+    "revision": "1ff1ec1ac9339dcf56c6",
+    "url": "/monters-and-stuff/static/js/runtime-main.21013988.js"
+  }
+]);
